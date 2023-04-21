0da6f56406eec7d466b88fbeb84627649a837fc1
aricionur <aricionur@gmail.com>
2023-04-21 18:34:09 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `foodstyles-challange-frontend`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
